52a4878d5f42cd2fda7d2be0ce6182e1611e12c8
Hadrien Mary <hadrien.mary@gmail.com>
2019-07-26 16:43:25 +0000

    git 2.0 in script

--- scripts/CentOS_7_Docker_Setup.sh
+++ scripts/CentOS_7_Docker_Setup.sh
@@ -13,7 +13,11 @@ sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum erase docker-engine-selinux
 sudo yum -y update
-sudo yum -y install docker-ce git
+sudo yum -y install docker-ce
+
+sudo yum -y remove git*
+sudo yum -y install  https://centos7.iuscommunity.org/ius-release.rpm
+sudo yum -y install  git2u-all
 
 sudo yum -y install firewalld
 sudo systemctl enable firewalld
