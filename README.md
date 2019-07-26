61c2d0feffbff459815dea2e24820ff078f48fd8
Hadrien Mary <hadrien.mary@gmail.com>
2019-07-26 16:08:53 +0000

    some refactoring

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ This instructions works for Centos 7 but any Linux system with Docker installed 
 
 ## Features
 
+- Multiple Wordpress instances.
 - Automatic backup in 7z format.
 - Automatic upload to Google Drive.
 - Automatic HTTPS certificates with Let's Encrypt.
@@ -24,7 +25,6 @@ bash "CentOS_7_Docker_Setup.sh"
 
 ```bash
 git clone https://github.com/hadim/docker-wordpress.git
-
 ```
 
 - Copy the example folder to the parent of `docker-wordpress`:
@@ -33,10 +33,10 @@ git clone https://github.com/hadim/docker-wordpress.git
 cp -R docker-wordpress/containers/website1/ .
 
 # Rename it to the domain name you want to use.
-mv website1/ websitename/
+mv website1/ my-website.com/
 
 # Set `PROJECT_PATH`.
-cd my-website.local/
+cd my-website.com/
 export PROJECT_PATH=$(pwd)
 ```
 
@@ -50,7 +50,7 @@ mkdir -p "$PROJECT_PATH/../pydrive"
 - Install crontab rule:
 
 ```bash
-(crontab -l 2>/dev/null; echo "30 6 * * * cd $PROJECT_PATH && python36 scripts/Upload_Backup_To_Drive.py") | crontab -
+(crontab -l 2>/dev/null; echo "30 6 * * * cd $PATH_TO_DOCKER_WORDPRESS && python36 scripts/Upload_Backup_To_Drive.py") | crontab -
 ```
 
 - Put your `settings.yaml` and  `client_secret.json` files into `$PROJECT_PATH/../pydrive`.
@@ -58,12 +58,9 @@ mkdir -p "$PROJECT_PATH/../pydrive"
 
 ```json
 {
-  "yourwebsite.com": "1FuYWdjsiRfbFBz-WoQll8sddsOPAMdlld7f8p5VL"
+  "my-website.com": "1FuYWdjsiRfbFBz-WoQll8sddsOPAMdlld7f8p5VL"
 }
 ```
-
-- In `containers/`, each subfolder corresponds to a Wordpress website. Duplicate subfolders as needed.
-- You can also edit also `containers/prod.sh`.
 
 ## Usage
 
@@ -74,7 +71,7 @@ cd "$PROJECT_PATH/../docker-wordpress"
 docker-compose pull
 docker network create nginx-proxy
 docker-compose up -d
-docker-compose logs -f 
+docker-compose logs -f
 # And check logs are ok.
 ```
 
@@ -82,27 +79,23 @@ docker-compose logs -f
 
 ```bash
 cd "$PROJECT_PATH"
-source ../docker-wordpress/containers/setup.sh example.com
+source ../docker-wordpress/containers/setup.sh my-website.com
 docker-compose build
 docker-compose up -d
-docker-compose logs -f 
+docker-compose logs -f
 # And check logs are ok.
 ```
 
 - Manual backup:
 
 ```bash
-cd "$PROJECT_PATH/containers/yourwebsite.com"
-source ../prod.sh
-source base.sh
+cd "$PROJECT_PATH"
 docker-compose exec wp-server /Backup.sh
 ```
 
 - Get a Bash into the Wordpress server:
 
 ```bash
-cd "$PROJECT_PATH/containers/yourwebsite.com"
-source ../prod.sh
-source base.sh
+cd "$PROJECT_PATH"
 docker-compose exec wp-server bash
 ```
--- wp-server/scripts/Start.sh
+++ wp-server/scripts/Start.sh
@@ -1,32 +1,17 @@
 #!/usr/bin/env bash
 
-chown -R nginx:nginx /data
+chmod +x /Backup.sh
 
 # Init wwww dir
-if [ ! -d /data/htdocs ] ; then
-  mkdir -p /data/htdocs
+if [ ! -d $WORDPRESS_INSTALL_DIR ] ; then
+  mkdir -p $WORDPRESS_INSTALL_DIR
 fi
-
-# Init php-fpm
-mkdir -p /data/logs/php-fpm
 
 # Init nginx
-mkdir -p /data/logs/nginx
 mkdir -p /tmp/nginx
-
 chown nginx:nginx /tmp/nginx
-chown -R nginx:nginx /data
-
-# Init wp-cli
-if [ ! -d /data/bin ] ; then
-  mkdir /data/bin
-  chown nginx:nginx /data/bin
-  cp /usr/bin/wp-cli /data/bin/wp-cli
-
-  curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-  chmod +x wp-cli.phar
-  mv wp-cli.phar /data/bin/wp-cli
-fi
+chown -R nginx:nginx $(dirname $WORDPRESS_INSTALL_DIR)
+mkdir -p /var/log/nginx
 
 # Set permissions to backup dir
 chown -R nginx:nginx $BACKUP_PARENT_DIR
@@ -34,12 +19,13 @@ chmod 777 -R $BACKUP_PARENT_DIR
 
 # Add cron job to do backup every night at 4:30am
 echo "* Set crontab job for backups"
-(crontab -l 2>/dev/null; echo '30 4 * * * su nginx -c "bash /Backup.sh"') | crontab -
+crontab -r
+(crontab -l 2>/dev/null; echo '30 4 * * * "./Backup.sh >> /var/log/backup.log 2>&1"') | crontab -
 
 # Wait for database to be ready
 while ! mysqladmin ping -h $DB_HOST --silent; do
-    echo "* Waiting for database server to be up."
-    sleep 5s;
+  echo "* Waiting for database server to be up."
+  sleep 5s;
 done
 
 echo "* Database is now ready."
@@ -49,31 +35,31 @@ su nginx -c "bash /Restore.sh"
 
 # Init wordpress directory if no backup restored
 if [ $? -ne 0 ]; then
-    if [ -z "$(ls -A $WORDPRESS_INSTALL_DIR)" ]; then
+  if [ -z "$(ls -A $WORDPRESS_INSTALL_DIR)" ]; then
 
-       echo "* Install Wordpress in $WORDPRESS_INSTALL_DIR"
+    echo "* Install Wordpress in $WORDPRESS_INSTALL_DIR"
 
-       cd $WORDPRESS_INSTALL_DIR
-       su nginx -c "wp-cli core download --locale=$WP_LOCALE --version=$WP_VERSION"
+    cd $WORDPRESS_INSTALL_DIR
+    su nginx -c "wp-cli core download --locale=$WP_LOCALE --version=$WP_VERSION"
 
-       su nginx -c "wp-cli core config --dbname=$DB_DATABASE --dbuser=$DB_USER \
-                                       --dbpass=$DB_PASSWORD --dbhost=$DB_HOST \
-                                       --dbprefix=$WP_DB_PREFIX"
+    su nginx -c "wp-cli core config --dbname=$DB_DATABASE --dbuser=$DB_USER \
+                                    --dbpass=$DB_PASSWORD --dbhost=$DB_HOST \
+                                    --dbprefix=$WP_DB_PREFIX"
 
-       su nginx -c "wp-cli core install --url=$MAIN_VIRTUAL_HOST \
-                                        --title=$WP_TITLE \
-                                        --admin_user=$WP_ADMIN_USER \
-                                        --admin_password=$WP_ADMIN_PASSWORD \
-                                        --admin_email=$WP_ADMIN_EMAIL"
+    su nginx -c "wp-cli core install --url=$MAIN_VIRTUAL_HOST \
+                                    --title=$WP_TITLE \
+                                    --admin_user=$WP_ADMIN_USER \
+                                    --admin_password=$WP_ADMIN_PASSWORD \
+                                    --admin_email=$WP_ADMIN_EMAIL"
 
-    fi
+  fi
 fi
 
 # Set site url
 su nginx -c "bash /Set_Site_URL.sh"
 
 # Remove wordpress cache
-rm -fr /var/www/wordpress/wp-content/cache/
+rm -fr $WORDPRESS_INSTALL_DIR/wp-content/cache/
 
 # If using SSL add some code to wp-config.php to handle this correctly
 if [[ "$SITE_URL" =~ "https" ]]; then
@@ -94,6 +80,14 @@ EOF
   echo $NEW_SSL_CONFIG | cat - $CONFIG_PATH > temp && mv temp $CONFIG_PATH
 
 fi
+
+# Forward request and error logs to docker log collector.
+echo "* Redirect nginx logs"
+chown -R nginx:nginx /var/log
+su nginx -c "ln -sf /dev/stdout /var/log/nginx_access.log"
+su nginx -c "ln -sf /dev/stderr /var/log/nginx_error.log"
+su nginx -c "ln -sf /dev/stderr /var/log/php-fpm.log"
+su nginx -c "ln -sf /dev/stdout /var/log/backup.log"
 
 echo "* Launch php-fpm7"
 php-fpm7
